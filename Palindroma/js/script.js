0753f1c450ab98214b5ea1571a1932b7f7dd4e0a
Emanuele213 <vittorius213@gmail.com>
2022-10-28 16:10:31 +0200

    exrcise palindrome completed

---
+++ Palindroma/js/script.js
@@ -0,0 +1,16 @@
+const word2 = prompt('Inserisci una parola: ');
+
+function checkPalindrome(word) {
+    const char = word.length;
+    for (let i = 0; i < char / 2; i++) {
+
+        if (word[i] !== word[char - 1 - i]) {
+            return 'E\' una parola palindroma';
+        }
+    }
+    return 'La parola è palindroma';
+}
+
+const output = checkPalindrome(word2);
+
+console.log(output);
